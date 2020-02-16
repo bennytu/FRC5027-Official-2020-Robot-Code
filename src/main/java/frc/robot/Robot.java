58d1f4f867817c5ae74842b77839ab57f0670746
TiffanyT171 <59751370+TiffanyT171@users.noreply.github.com>
2020-02-16 15:57:09 -0800

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,13 +15,15 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 //import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 //import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.Timer;
+// import com.ctre.phoenix.motorcontrol.can.*;
 
 
 /**
@@ -38,15 +40,32 @@ public class Robot extends TimedRobot {
     public static final int can2 = 2;
     public static final int can3 = 3;
     public static final int can4 = 4;
+    public static final int can5 = 5;
+    public static final int can6 = 6;
+    public static final int buttonX = 0;
+    public static final int buttonA = 1;
+    public static final int buttonB = 2;
+    public static final int buttonY = 3;
 
     Joystick joy1 = new Joystick(4); //inputs for Joystick
-
-    VictorSPX leftFrontMotor = new VictorSPX(can1);
+    JoystickButton LowPowerThrow = new JoystickButton(joy1, buttonX); 
+    JoystickButton MediumPowerThrow = new JoystickButton(joy1, buttonA);
+    JoystickButton MediumHighPowerThrow = new JoystickButton(joy1, buttonB);
+    JoystickButton HighPowerThrow = new JoystickButton(joy1, buttonY);
+    WPI_VictorSPX leftFrontMotor = new WPI_VictorSPX(can1);
+    WPI_VictorSPX rightFrontMotor = new WPI_VictorSPX(can3);
+    WPI_VictorSPX leftRearMotor = new WPI_VictorSPX(can2);
+    WPI_VictorSPX rightRearMotor = new WPI_VictorSPX(can4);
+    WPI_VictorSPX shooter_leftmotor = new WPI_VictorSPX(can5);
+    WPI_VictorSPX shooter_rightmotor = new WPI_VictorSPX(can6);
+    DifferentialDrive _drive = new DifferentialDrive(leftFrontMotor, rightFrontMotor);
+    double powerLevel = 0.0;
+  /*  VictorSPX leftFrontMotor = new VictorSPX(can1);
     VictorSPX leftRearMotor  = new VictorSPX(can2);
 
     VictorSPX rightFrontMotor = new VictorSPX(can3);
-    VictorSPX rightRearMotor  = new VictorSPX(can4);;
-
+    VictorSPX rightRearMotor  = new VictorSPX(can4);
+*/
     //SpeedControllerGroup leftMotorGroup;
     //SpeedControllerGroup rightMotorGroup;
     //DifferentialDrive m_drive;
@@ -99,7 +118,7 @@ public class Robot extends TimedRobot {
       else 
       {
         final double stick1 = 0.0;
-        final double xcontrol = 0.0;
+      //  final double xcontrol = 0.0;
         double leftMotorPower = stick1;
         double rightMotorPower = stick1 * -1;
         leftFrontMotor.set(ControlMode.PercentOutput, leftMotorPower);
@@ -115,15 +134,60 @@ public class Robot extends TimedRobot {
     @Override
 
   public void teleopInit() {
+  /* factory default values */
+  leftFrontMotor.configFactoryDefault();
+  rightFrontMotor.configFactoryDefault();
 
-    final double turnSpeed = joy1.getX();
-    final double fbSpeed = joy1.getY(); 
+  /* flip values so robot moves forward when stick-forward/LEDs-green */
+  leftFrontMotor.setInverted(false); // <<<<<< Adjust this
+  rightFrontMotor.setInverted(true); // <<<<<< Adjust this
+
+  /*
+   * WPI drivetrain classes defaultly assume left and right are opposite. call
+   * this so we can apply + to both sides when moving forward. DO NOT CHANGE
+   */
+  _drive.setRightSideInverted(false);
+   /* final double turnSpeed = joy1.getX();
+    final double fbSpeed = joy1.getY(); */
 
   }
 
   @Override
 
   public void teleopPeriodic() {
+    double xSpeed = joy1.getRawAxis(1) * -1; // make forward stick positive
+    double zRotation = joy1.getRawAxis(2); // WPI Drivetrain uses positive=> right
+    boolean lowPowerThrow = joy1.getRawButton(buttonX);
+    boolean MediumPowerThrow = joy1.getRawButton(buttonA);
+    boolean MediumHighPowerThrow = joy1.getRawButton(buttonB);
+    boolean HighPowerThrow = joy1.getRawButton(buttonY);
+
+    _drive.arcadeDrive(xSpeed, zRotation);
+
+    /* hold down btn1 to print stick values */
+    if (joy1.getRawButton(1)) {
+      System.out.println("xSpeed:" + xSpeed + "    zRotation:" + zRotation);
+      }
+
+    if(lowPowerThrow) {
+      // Run shooter motors at low speed
+      powerLevel = 0.25;
+    } else if(MediumPowerThrow) {
+      // Run shooter motors at medium speed
+     powerLevel = 0.5;
+    } else if(MediumHighPowerThrow) {
+      // Run shooter motors at medium-high speed
+     powerLevel = 0.75;
+    } else if(HighPowerThrow) {
+      // Run shooter motors at high speed
+     powerLevel = 1.0; 
+      }
+      double shooter_leftmotor_power = powerLevel;
+      double shooter_rightmotor_power = powerLevel * -1;
+      shooter_leftmotor.set(ControlMode.PercentOutput, shooter_leftmotor_power);
+      shooter_rightmotor.set(ControlMode.PercentOutput, shooter_rightmotor_power);
+    }
+    /*
     final double stick1 = joy1.getRawAxis(1);
     final double xcontrol = joy1.getRawAxis(1);
     double leftMotorPower = stick1;
@@ -162,7 +226,7 @@ public class Robot extends TimedRobot {
   }
 
 
-
+*/
     @Override
 
     public void testInit() {
